03d6824350f6f5b41e797487783084f13eef30b2
kss <kss07120@gmail.com>
2021-01-05 00:16:48 +0900

    rename file

--- src/main/java/com/zerowaste/ZeroWasteApplication.java
+++ src/main/java/com/zerowaste/ZeroWasteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZerowasteApplication {
+public class ZeroWasteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZerowasteApplication.class, args);
+		SpringApplication.run(ZeroWasteApplication.class, args);
 	}
 
 }
