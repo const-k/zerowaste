d9a7ed644519249ad6ad68dd020d5349b42efe28
kss <kss07120@gmail.com>
2021-01-05 00:13:55 +0900

    rename package

--- src/main/java/com/zerowaste/ZerowasteApplication.java
+++ src/main/java/com/zerowaste/ZerowasteApplication.java
@@ -1,4 +1,4 @@
-package com.zerowaste.shop.zerowaste;
+package com.zerowaste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZerowasteApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZerowasteApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ZerowasteApplication.class, args);
+	}
 
 }
--- src/test/java/com/zerowaste/ZerowasteApplicationTest.java
+++ src/test/java/com/zerowaste/ZerowasteApplicationTest.java
@@ -1,4 +1,4 @@
-package com.zerowaste.shop.zerowaste;
+package com.zerowaste;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
