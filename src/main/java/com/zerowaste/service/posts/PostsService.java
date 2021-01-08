c4d06605fbac009f667d5e56a67a80e1ba16f6ab
ss0712.kim <ss0712.kim@samsung.com>
2021-01-08 19:25:30 +0900

    finished crud board

--- src/main/java/com/zerowaste/service/posts/PostsService.java
+++ src/main/java/com/zerowaste/service/posts/PostsService.java
@@ -13,19 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
 
-    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
-    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
@@ -33,15 +31,22 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
     public List<PostsListReponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListReponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        Posts post = postsRepository.findById(id).orElseThrow(() -> new IllegalStateException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(post);
     }
 }
