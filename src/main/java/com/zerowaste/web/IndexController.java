a38be79e198fb3570c3e22cbeb0391a67d71d95d
ss0712.kim <ss0712.kim@samsung.com>
2021-01-11 19:38:56 +0900

    add annotation sessionUser

--- src/main/java/com/zerowaste/web/IndexController.java
+++ src/main/java/com/zerowaste/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.zerowaste.web;
 
+import com.zerowaste.config.auth.LoginUser;
 import com.zerowaste.config.auth.dto.SessionUser;
 import com.zerowaste.service.posts.PostsService;
 import com.zerowaste.web.dto.PostsResponseDto;
@@ -8,19 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) { // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 함
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 함
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
